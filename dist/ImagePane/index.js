6c792e6967d2ab2aa1128d0fe87842b2e38b48ee
Ryan Yang <ryanqyang@gmail.com>
2022-02-04 10:32:18 -0800

    Compiled new dist

--- dist/ImagePane/index.js
+++ dist/ImagePane/index.js
@@ -5,7 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _ImagePane = _interopRequireDefault(require("./ImagePane.jsx"));
+var _ImagePane = _interopRequireDefault(require("./ImagePane.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
