d624dd1965ce1537c77953607aa5283398d93fe3
Ryan Yang <ryanqyang@gmail.com>
2022-02-04 10:12:04 -0800

    added mobile format. ready for prod

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,13 @@ function App() {
 				src='https://i.pinimg.com/736x/76/93/59/76935964bca553da191831f6b800d262.jpg'
 				alt='Girl in Gray'
 				content={{
-					tag: <>esta<wbr />blishments</>,
+					tag: <>establi<wbr />shments</>,
 					title: 'girl in gray',
 					description:
 						'One of the more notable works of the photographer. Taken in gray. Superposed upon the city scape - the details attract.',
 				}}
 			/>
+			
 			<ImagePane
 				className={cn.image}
 				src='https://boba.watch/bobawatch.png'
